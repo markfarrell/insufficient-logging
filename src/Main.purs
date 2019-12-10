159cad1c669a9edd756b3319023eabee3769455f
Mark Farrell <m4farrel@uwaterloo.ca>
2019-12-10 07:08:56 +0000

    runRoute, responseRoute

--- src/Main.purs
+++ src/Main.purs
@@ -1,53 +1,155 @@
-module Main where
-
+module Main where 
+  
 import Prelude
 
+import Control.Alt ((<|>))
+import Control.Coroutine (Producer, Consumer, Process, pullFrom, await, runProcess)
+import Control.Coroutine.Aff (produce, emit)
+import Control.Monad.Error.Class (try)
+import Control.Monad.Rec.Class (forever)
 import Control.Monad.Trans.Class (lift)
+
 import Data.Either(Either(..))
+import Data.Tuple(Tuple(..))
 
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff)
 import Effect.Console (log) as Console
 import Effect.Class (liftEffect)
 
+import Data.Traversable(foldMap)
+import Data.List(many)
+import Data.String.CodeUnits (singleton)
+
+import Text.Parsing.Parser (Parser, runParser)
+import Text.Parsing.Parser.String (string, anyChar)
+
 import DB as DB
+import HTTP as HTTP
 import SQLite3 as SQLite3
 
-data Severity = Success | Failure
+data MessageType = Success | Failure
 
-data Message = Message Severity String
+data MessageID = DatabaseRequest | RouteRequest
 
-instance showSeverity :: Show Severity where
+data Message = Message MessageType MessageID String
+
+instance showMessageType :: Show MessageType where
   show Success = "SUCCESS"
   show Failure = "FAILURE"
 
-log :: String -> Aff Unit
-log = liftEffect <<< Console.log
+instance showMessageID :: Show MessageID where
+  show DatabaseRequest = "DATABASE-REQUEST"
+  show RouteRequest    = "ROUTE-REQUEST"
 
-insertMessage :: SQLite3.Database -> Message -> DB.Request Unit
-insertMessage database (Message severity msg) = do
-  _ <- DB.all query database
-  lift $ pure unit
-  where query = "INSERT INTO Messages (severity, msg) VALUES (" <> show severity <> ", " <> msg <> ")"
-
-createMessages :: SQLite3.Database -> DB.Request Unit
-createMessages database = do
-  _ <- DB.all query database
-  lift $ pure unit
-  where query = "CREATE TABLE Messages (severity TEXT NOT NULL, msg TEXT NOT NULL)"
-
-openReadWrite :: String -> DB.Request SQLite3.Database
-openReadWrite filename = do
+instance showMessage :: Show Message where
+  show (Message ty id msg) = "Message " <> show [show ty, show id, msg] 
+ 
+audit :: Message -> Aff Unit
+audit = liftEffect <<< Console.log <<< show
+  
+insertMessage :: String -> Message -> DB.Request Unit
+insertMessage filename (Message ty id msg) = do
   database <- DB.connect filename SQLite3.OpenReadWrite
-  lift $ pure database
+  _ <- DB.all query database
+  _ <- DB.close database
+  lift $ pure unit
+  where query = "INSERT INTO Messages (type,id,msg) VALUES (" <> show [show ty, show id, msg] <> ")" 
 
-initialize :: String -> DB.Request SQLite3.Database
-initialize filename = do
-  database <- openReadWrite filename
-  _ <- createMessages database
-  lift $ pure database
+data Route = InsertMessage Message
+ 
+instance showRoute :: Show Route where
+  show (InsertMessage message) = "InsertMessage (" <> show message <> ")"
+
+parseMessageType :: Parser String MessageType
+parseMessageType = do
+  _ <- string "type"
+  _ <- string "="
+  parseSuccess <|> parseFailure
+  where 
+    parseSuccess = string (show Success) >>= const (pure Success)
+    parseFailure = string (show Failure) >>= const (pure Failure)
+
+parseMessageID :: Parser String MessageID
+parseMessageID = do
+  _ <- string "id"
+  _ <- string "="
+  parseDatabaseRequest <|> parseRouteRequest
+  where
+    parseDatabaseRequest = string (show DatabaseRequest) >>= const (pure DatabaseRequest)
+    parseRouteRequest = string (show RouteRequest) >>= const (pure RouteRequest)
+
+parseMessageString :: Parser String String
+parseMessageString = do
+  _ <- string "msg"
+  _ <- string "="
+  foldMap singleton <$> many anyChar
+
+parseMessageQuery :: Parser String Message
+parseMessageQuery = do
+  ty <- parseMessageType
+  _ <- string "&"
+  id <- parseMessageID
+  _ <- string "&"
+  msg <- parseMessageString
+  pure $ Message ty id msg
+
+parseInsertMessage :: Parser String Route
+parseInsertMessage = do
+  _ <- string "/insert/message"
+  _ <- string "?"
+  message <- parseMessageQuery
+  pure (InsertMessage message)
+
+parseRoute :: Parser String Route
+parseRoute = parseInsertMessage
+
+data ContentType a = TextHTML a
+
+data ResponseType a = Ok (ContentType a) | BadRequest
+
+instance showContentType :: (Show a) => Show (ContentType a) where
+  show (TextHTML x) = "TextHTML (" <> show x <> ")"
+
+instance showResponseType :: (Show a) => Show (ResponseType a) where
+  show (Ok x)       = "Ok (" <> show x <> ")"
+  show (BadRequest) = "BadRequest"
+
+runRoute :: HTTP.IncomingMessage -> Aff (ResponseType Route)
+runRoute req  = do
+  result <- pure $ flip runParser parseRoute $ HTTP.messageURL req
+  case result of
+    (Left error)  -> do
+      pure $ BadRequest
+    (Right route) -> do
+      pure $ Ok (TextHTML route)
+ 
+respondRoute :: ResponseType Route -> HTTP.ServerResponse -> Aff Unit
+respondRoute (Ok (TextHTML (InsertMessage message))) = \res -> liftEffect $ do
+  _ <- HTTP.setHeader "Content-Type" "text/html" $ res
+  _ <- HTTP.writeHead 200 $ res
+  _ <- HTTP.write body $ res
+  _ <- HTTP.end $ res
+  pure unit
+  where body = show (InsertMessage message)
+respondRoute (BadRequest) = \res -> liftEffect $ do
+  _ <- HTTP.writeHead 400 $ res
+  _ <- HTTP.end $ res
+  pure unit  
+
+producer :: HTTP.Server -> Producer (Tuple HTTP.IncomingMessage HTTP.ServerResponse) Aff Unit
+producer server = produce \emitter -> do
+  HTTP.onRequest (\req res -> emit emitter $ Tuple req res) $ server
+
+consumer :: Consumer (Tuple HTTP.IncomingMessage HTTP.ServerResponse) Aff Unit
+consumer = forever $ do
+  request <- await
+  case request of
+    Tuple req res -> do
+      routeResult <- lift $ try (runRoute req)
+      case routeResult of
+        (Left  error)        -> lift $ audit $ Message Failure RouteRequest (show error)
+        (Right responseType) -> lift $ audit $ Message Success RouteRequest (show responseType)
 
 main :: Effect Unit
-main = void $ launchAff $ do
-  result <- DB.runRequest $ initialize ":memory:"
-  log $ show result
+main = void $ launchAff $ pure unit
